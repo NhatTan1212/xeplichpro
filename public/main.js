d94ada55685ebb95366d01083bc7be1ea6cb42dc
NhatTan1212 <116484948+NhatTan1212@users.noreply.github.com>
2023-03-19 14:53:47 +0700

    Update main.js

--- public/main.js
+++ public/main.js
@@ -67,7 +67,7 @@
                 // a.download = "data.json";
                 // a.click();
                 var xhr = new XMLHttpRequest();
-                xhr.open('POST', 'http://localhost:3000/', true);
+                xhr.open('POST', 'https://nhattan1212.github.io/xeplichpro/', true);
                 xhr.setRequestHeader('Content-Type', 'application/json');
                 xhr.onreadystatechange = function() {
                 if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
